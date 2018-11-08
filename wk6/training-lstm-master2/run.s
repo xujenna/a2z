1232701a41a451209245f4400b898140d47e5930
Jenna Xu <jxu2@Jennas-MacBook-Pro-2.local>
2018-11-07 21:52:37 -0500

    changed run script

--- wk6/training-lstm-master2/run.s
+++ wk6/training-lstm-master2/run.s
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --gres=gpu:4 -c4
+#SBATCH --gres=gpu:3 -c3
 #SBATCH --time=05:00:00
 #SBATCH --mem=240GB
-#SBATCH --job-name=happyDB9
+#SBATCH --job-name=happyDB11_smaller
 #SBATCH --mail-type=END
 #SBATCH --mail-user=jx603@nyu.edu
 #SBATCH --output=slurm_%j.out
@@ -16,10 +16,10 @@ module load numpy/python3.6/intel/1.14.0 tensorflow/python3.6/1.5.0
 cd /scratch/jx603/training-lstm-master2
 
 python train.py --data_dir=./data \
---rnn_size 1024 \
---num_layers 3 \
+--rnn_size 128 \
+--num_layers 2 \
 --seq_length 256 \
 --batch_size 64 \
---num_epochs 50 \
+--num_epochs 25 \
 --save_checkpoints ./checkpoints \
 --save_model ./models
