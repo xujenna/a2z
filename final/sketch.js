8997666d979f4204cc7e767328b495c6cb5eaea1
Jenna Xu <jxu2@10-17-93-191.dynapool.wireless.nyu.edu>
2018-11-28 21:45:06 -0500

    tsne mapping, d3

--- final/sketch.js
+++ final/sketch.js
@@ -46,12 +46,28 @@ class Word2Vec {
 };
 
 
+
+
 let word2vec = new Word2Vec('data/wordvecs10000.json', modelLoaded);
 const SpeechRecognition = webkitSpeechRecognition;
 const synth = window.speechSynthesis;
-playerOneScore = 0;
+let playerOneScore = 0;
+let data;
+let data_keys;
+let counter;
+let tsneOpt;
+let tsneCoordinates;
 
+let plotSVG = d3.select("#tsneSVG")
+.append("svg:svg")
+  .attr("width",1300)
+  .attr("height",800);
 
+// let canvas = document.getElementById("tsneCanvas");
+// let ctx = canvas.getContext("2d");
+// ctx.font = "18px Arial";
+// ctx.fillText("hello",100,300);
+// ctx.fillStyle = "#FF0000";
 
 const speak = (textInput) => {
   if(synth.speaking){
@@ -64,25 +80,51 @@ const speak = (textInput) => {
 
 
 function modelLoaded() {
+  document.getElementById('start-button').textContent = "Computing t-SNE...";
+  tsnefy();
+}
+
+async function tsnefy() {
+  data = Object.keys(word2vec.model).map(k => Array.prototype.slice.call(word2vec.model[k].dataSync()));
+  data_keys = Object.keys(word2vec.model);
+  word2vec_tsne_tensor = tf.tensor2d(data);
+  tsneOpt = tsne.tsne(word2vec_tsne_tensor);
+  await tsneOpt.compute().then(() => {
+     tsneCoordinates = tsneOpt.coordsArray();
+  })
   document.getElementById('start-button').innerHTML= "<button onclick='start();'>Start Game</button>";
 }
 
-// var randomProperty = function (obj) {
-function randomProperty(obj) {
-  var keys = Object.keys(obj)
+// function randomProperty(obj) {
+//   var keys = Object.keys(obj)
 
-  return keys[ keys.length * Math.random() << 0];
-};
+//   return keys[keys.length * Math.random() << 0];
+// };
 
-async function start() {
-  document.querySelector('#start-button > button').disabled = true;
+function start() {
+  // document.querySelector('#start-button > button').disabled = true;
+  document.querySelector('#start-button > button').style.display = "none";
+  document.querySelector('#scoring').style.display = "block";
+  // document.getElementById('currentWord').textContent = "";
+  document.getElementById('lastRoundPoints').textContent = 0;
+  document.getElementById('lastRoundTimePenalty').textContent = 0;
+  document.getElementById('playerOneScore').textContent = 0;
+  // ctx.clearRect(0,0,canvas.width, canvas.height);
+  // ctx.beginPath();
+  // plotSVG.parentNode.replaceChild(plotSVG.cloneNode(false), plotSVG);
+  plotSVG.remove();
+  plotSVG = d3.select("#tsneSVG")
+  .append("svg:svg")
+    .attr("width",1300)
+    .attr("height",800);
+
+  // document.querySelector('#tsneCanvas').style.display = "block";
   playerOneScore = 0;
+  // let starter = randomProperty(word2vec.model)
+  let starter = data_keys[data_keys.length * Math.random() << 0]
 
-  let starter = await randomProperty(word2vec.model)
-  
-
-  document.getElementById('currentWord').textContent = starter;
-
+  // document.getElementById('currentWord').textContent = starter;
+  getCoordinates(starter);
   speak(starter);
   newTurn(starter);
 }
@@ -90,13 +132,11 @@ async function start() {
 
 function newTurn(word) {
   let count = 0;
-  let counter = setInterval(function() {
+  counter = setInterval(function() {
     count += 1;
     document.getElementById('countDown').textContent = 7-count;
-    if(count >= 7){
-      clearInterval(counter);
-      document.getElementById('countDown').textContent = "Game Over";
-      document.querySelector('#start-button > button').disabled = false;
+    if(count >= 6){
+      gameOver();
     }
   }, 1000);
 
@@ -110,18 +150,31 @@ function newTurn(word) {
     clearInterval(counter);
 
     let newWord = event.results[0][0].transcript;
-    console.log(newWord)
-    document.getElementById('currentWord').textContent = newWord;
+    newWord = newWord.toLowerCase();
+    // console.log(newWord)
+    // document.getElementById('currentWord').textContent = newWord;
 
-    let newScore = Math.round(Math.pow((distance(word, newWord)*10),3)) - timePenalty;
-    console.log(newScore)
-    document.getElementById('lastRoundScore').textContent = newScore + timePenalty;
-    document.getElementById('lastRoundTimePenalty').textContent = timePenalty;
+    try {
+      let wordDistance = distance(word, newWord);
+      let newScore = Math.round(Math.pow((wordDistance*10),3)) - timePenalty;
+      console.log(newScore)
+      // document.getElementById('lastRoundScore').textContent = newScore;
+      document.getElementById('lastRoundPoints').textContent = newScore + timePenalty;
+      document.getElementById('lastRoundTimePenalty').textContent = timePenalty;
+      getCoordinates(newWord);
 
-    playerOneScore += newScore;
-    document.getElementById('playerOneScore').textContent = playerOneScore;
+      playerOneScore += newScore;
+      document.getElementById('playerOneScore').textContent = playerOneScore;
 
-    newTurn(newWord)
+      newTurn(newWord)
+
+    }
+    catch(err){
+      console.log("error: " + err)
+      document.getElementById('tryAgain').textContent = "Try again (" + newWord + " not in model)";
+      newTurn(word)
+    }
+
   }
 
   recognition.onend = () => {
@@ -134,27 +187,68 @@ function newTurn(word) {
   }
 }
 
-function getCoordinates(word) {
-  // console.log(word2vec.model[word])
-  let tsneOpt = tsne.tsne(word2vec.model[word]);
-  // tsneOpt.coordsArray().then(result=>{
-  //   console.log("hellow");
-  //   console.log(result)
-  //   map(result)
-  // })
-  tsneOpt.compute().then(() => {
-    const coordinates = tsneOpt.coordinates();
-    coordinates.print();
-    // let coordinates = tsneOpt.coordsArray().then(result=>{
-    //     console.log("hellow");
-    //     console.log(result)
-    //     map(result)
-    //   })
-  })
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+let prev_xCoord;
+let prev_yCoord;
+
+
+async function getCoordinates(word) {
+  let wordIndex = data_keys.indexOf(word);
+  console.log(wordIndex);
+  words = document.querySelectorAll("text");
+  words.forEach(function(d){d.classList.replace("newEmbeddingText", "embeddingText")})
+
+  // let wordIndex = data.indexOf(Array.prototype.slice.call(word2vec.model[word].dataSync()));
+  await tsneCoordinates.then(function(result) {
+    console.log(result[wordIndex]);
+    let xCoord = scale(result[wordIndex][0], 0, 1, 0, 1200);
+    let yCoord = scale(result[wordIndex][1], 0, 1, 0, 800);
+    console.log(xCoord)
+    console.log(yCoord)
+    // ctx.font = "18px monospace";
+    // ctx.fillText(word,xCoord,yCoord);
+    // ctx.fillStyle = "#FF0000";
+    plotSVG.append("text")
+      .text(word)
+      .attr("x", xCoord)
+      .attr("y", yCoord)
+      .attr("class", "newEmbeddingText");
+
+    try{
+      // ctx.moveTo(prev_xCoord,prev_yCoord);
+      // ctx.lineTo(xCoord,yCoord);
+      // ctx.stroke();
+      plotSVG.append("line")
+        .attr("x1", prev_xCoord)
+        .attr("y1", prev_yCoord)
+        .attr("x2", xCoord)
+        .attr("y2", yCoord)
+        .attr("stroke", "grey")
+        .attr("stroke-width", 1.5)
+        .attr("opacity", 0.3)
+        .attr("stroke-dasharray", 3)
+    }
+    finally{
+      prev_xCoord = xCoord;
+      prev_yCoord = yCoord;
+    }
+  });
 }
 
 function distance(a, b) {
-  let result = tf.util.distSquared(word2vec.model[a].dataSync(), word2vec.model[b].dataSync())
+  let result = tf.util.distSquared(word2vec.model[a].dataSync(), word2vec.model[b].dataSync());
   return result;
 }
 
+function gameOver(){
+  clearInterval(counter);
+  document.getElementById('countDown').textContent = "Game Over";
+  // document.querySelector('#start-button > button').disabled = false;
+  // document.querySelector('#start-button > button').style.display = "block";
+  // document.querySelector('#start-button > button').textContent = "Restart Game";
+  document.getElementById('restart-button').style.display= "inline-block";
+}
